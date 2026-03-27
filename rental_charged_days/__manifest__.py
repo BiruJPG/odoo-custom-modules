38e912944b013e7b73736a51e6404526ff65d543
BiruJPG <juancapurro@gmail.com>
2026-03-27 11:48:53 -0300

    Create __manifest__.py

---
+++ rental_charged_days/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Rental Charged Days',
+    'version': '19.0.1.0.0',
+    'summary': 'Agrega campo "Días Cobrados" independiente del período logístico',
+    'category': 'Rental',
+    'depends': ['rental'],
+    'data': [
+        'views/rental_order_views.xml',
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+}
